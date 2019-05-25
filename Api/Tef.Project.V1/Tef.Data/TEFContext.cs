5111b4111891a53c0cf40da0c30f9c0d673c008c
thanhnam72 <lythanhnam72@gmail.com>
2019-05-25 23:36:06 +0700

    add code first

---
+++ Api/Tef.Project.V1/Tef.Data/TEFContext.cs
@@ -0,0 +1,21 @@
+﻿using DP.V2.Core.Common.Base;
+using DP.V2.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tef.Data
+{
+    public class TEFContext : DataContext
+    {
+        public TEFContext(DbContextOptions<TEFContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.AddGlobalFilter<BaseEntity>(this.GetType());
+            modelBuilder.AddEntityCommon();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Api/Tef.Project.V1/Tef.Project.V1/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DP.V2.Core.WebApi.Configuration;
+using DP.V2.Core.Data;
+using Tef.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tef.Project.V1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            WebApiStartup.ApiConfigureServices(services, Configuration);
+            services.AddDbContext<TEFContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+            services.AddScoped(typeof(DataContext), typeof(TEFContext));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (AppSetting.GetObject<bool>("CreateDb"))
+            {
+                var context = app.ApplicationServices
+                    .GetService<IServiceScopeFactory>()
+                    .CreateScope()
+                    .ServiceProvider
+                    .GetRequiredService<TEFContext>();
+
+                context.Database.EnsureCreated();
+            }
+
+            WebApiStartup.Configure(app, env);
+        }
+    }
+}
