b084157bb49ab1b25ba9fde8c86d065039eebb66
thanhnam72 <lythanhnam72@gmail.com>
2019-07-09 23:45:30 +0700

    update cache

---
+++ Framework/DP.Core.V2-DS/DP.V2.Core/DP.V2.Core.Cache/ICachingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DP.V2.Core.Cache
+{
+    public interface ICachingService
+    {
+        T GetObject<T>(string cacheKey, int expireInMinute, Func<T> objectFunction);
+        T GetObject<T>(string cacheKey, Func<T> objectFunction);
+        void Remove(string cacheKey);
+    }
+}
---
+++ Framework/DP.Core.V2-DS/DP.V2.Core/DP.V2.Core.Cache/MemoryCacheService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace DP.V2.Core.Cache
+{
+    public class MemoryCacheService : ICachingService
+    {
+        private readonly IMemoryCache _cache;
+
+        public MemoryCacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public T GetObject<T>(string cacheKey, int expireInMinute, Func<T> objectFunction)
+        {
+            if (_cache.TryGetValue(cacheKey, out T cachedObject))
+            {
+                return cachedObject;
+            }
+
+            var options = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(expireInMinute),
+                SlidingExpiration = TimeSpan.FromMinutes(5)
+            };
+
+            cachedObject = objectFunction();
+
+            _cache.Set(cacheKey, cachedObject, options);
+
+            return cachedObject;
+        }
+
+        public T GetObject<T>(string cacheKey, Func<T> objectFunction)
+        {
+            return GetObject(cacheKey, 60, objectFunction);
+        }
+
+        public void Remove(string cacheKey)
+        {
+            _cache.Remove(cacheKey);
+        }
+    }
+}
